d507bc9e2276e6c19e1601fa0a16bec504bbb905
Dmitry Ermichev <dmitryermichev@gmail.com>
2018-11-13 16:01:44 +0300

    unit test example added

---
+++ tests/fixtures/data/article.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'article1' => [
+        'id' => 1,
+        'user_id' => 2,
+        'slug' => 'article-1',
+        'title' => 'Article 1',
+        'description' => 'Description 1',
+        'body' => 'Body 1',
+        'created_at' => '2000-01-01',
+        'updated_at' => '2000-01-01',
+    ]
+];
